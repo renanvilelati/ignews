0463005c2959b80621ccda1da996ba055ecede9e
Renan Vilela <renanvilelati@gmail.com>
2023-04-24 17:37:11 -0300

    Styling hero section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,27 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
+import { SubscribeButton } from '@/SubscribeButton'
 
 export default function Home() {
   return (
     <>
-      <Head> <title>Início | ig.news</title> </Head>
-    <h1> Oi </h1>
+      <Head> <title>Home | ig.news</title> </Head>
+    
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+            <span>😁 Hey, welcome</span>
+            <h1>News about the <span>React</span> world.</h1>
+
+            <p>
+              Get access to all the publications <br />
+              <span> for $9.98 month </span>
+            </p>
+
+            <SubscribeButton />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
 }
