7de6c92190200c05ca06099befe4f158d2a96832
Renan Vilela <renanvilelati@gmail.com>
2023-04-24 18:20:41 -0300

    Creating static page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,18 @@
+import { GetStaticProps } from 'next'
+
 import Head from 'next/head'
 import styles from '../styles/Home.module.scss'
 import { SubscribeButton } from '@/SubscribeButton'
+import { stripe } from '@/services/stripe'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string,
+    amount: number 
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head> <title>Home | ig.news</title> </Head>
@@ -14,14 +24,34 @@ export default function Home() {
 
             <p>
               Get access to all the publications <br />
-              <span> for $9.98 month </span>
+              <span> for {product.amount} month </span>
             </p>
 
-            <SubscribeButton />
+            <SubscribeButton priceId={product.priceId} />
         </section>
 
         <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+
+  const price = await stripe.prices.retrieve('price_1N0WCFFDKGnnqiIIzy57grqs')
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount! / 100),
+  }
+
+  return {
+    props: {
+      product
+    },
+    revalidate: 60 * 60 * 24 // 24 horas
+  }
 }
